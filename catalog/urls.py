62b71736c5dc33c0b9d1e2cf575464dfddd16914
maxim <mcsim55555k@gmail.com>
2026-06-06 01:23:15 +0300

    Additional task finished

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", views.home, name='home'),
     path("contacts/", views.contacts, name='contacts'),
+    path('feedback/', views.feedback, name='feedback'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     if request.method == 'GET':
@@ -8,3 +9,9 @@ def contacts(request):
     if request.method == 'GET':
         return render(request, 'contacts.html')
 
+def feedback(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваша информация получена.")
+    return render(request, 'feedback.html')
