56c0f1767a1c62f79e3ebb5339c58b5adbba5788
maxim <mcsim55555k@gmail.com>
2026-06-10 15:36:48 +0300

    Homework 23 task 2 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", views.home, name='home'),
-    path("contacts/", views.contacts, name='contacts'),
-    path('feedback/', views.feedback, name='feedback'),
+    path("", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("feedback/", views.feedback, name="feedback"),
 ]
