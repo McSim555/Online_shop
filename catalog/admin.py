fbc7e2948edc7c88106cca38b6b605f075c31a9c
maxim <mcsim55555k@gmail.com>
2026-06-11 19:37:09 +0300

    Homework 23 additional task done

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Contact
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -13,4 +13,9 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name', 'description')
 
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'country', 'city', 'street', 'house_number')
+    search_fields = ('country', 'city', 'street', 'house_number')
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product, Contact
 
 
 def home(request):
+    latest_products = Product.objects.order_by('created_at')[:5]
+    for product in latest_products:
+        print(f"Продукт: {product.name}, Цена: {product.price}, Дата создания: {product.created_at}")
+
     if request.method == "GET":
         return render(request, "home.html")
 
 
 def contacts(request):
     if request.method == "GET":
-        return render(request, "contacts.html")
+        try:
+            contact_shown = Contact.objects.get(name='Филиал 1')
+        except Contact.DoesNotExist:
+            contact_shown = None
+        except Contact.MultipleObjectsReturned:
+            contact_shown = Contact.objects.filter(name='Филиал 1').first()
+        return render(request, "contacts.html", {'contact': contact_shown})
 
 
 def feedback(request):
