f6e51dae56d4575ee585779947eb138376304570
maxim <mcsim55555k@gmail.com>
2026-07-02 00:00:50 +0300

    Homework 24 tasks 1 - 3 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
     path("feedback/", views.feedback, name="feedback"),
+    path("product/<int:pk>/", views.product_info, name="product_info"),
+    path("main/", views.products_info, name="products_info"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from catalog.models import Product, Contact
 
@@ -31,3 +31,15 @@ def feedback(request):
         message = request.POST.get("message")
         return HttpResponse(f"Спасибо, {name}! Ваша информация получена.")
     return render(request, "feedback.html")
+
+
+def product_info(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product.html", context)
+
+
+def products_info(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "main.html", context)
